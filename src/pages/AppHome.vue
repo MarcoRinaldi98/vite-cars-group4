9972919b925f71f11231a71cb6b7a00b4b404240
nicolo fossati <nicofossa01@gmail.com>
2023-06-05 16:51:26 +0200

    Home Completata

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Siamo nella HOME</h1>
+</template>
+
+<script>
+
+export default {
+    name: 'AppHome'
+}
+</script>
+
+<style lang="scss"></style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AppHome from './pages/AppHome.vue';
 
 const router = createRouter(
     {
         history: createWebHistory(),
         routes: [
-
+            {
+                path: '/',
+                name: 'home',
+                component: AppHome
+            }
         ]
     }
 );
